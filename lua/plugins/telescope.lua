006744c805daf812b6fd2037e6e4a17d86b1c945
Jérémie Bolduc <jeremiebolducjs@gmail.com>
2025-09-11 09:36:37 -0400

    added telescope fzf native extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,35 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
+	},
 	config = function()
+		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
 
+		telescope.setup({
+			defaults = {
+				-- use ripgrep for better performance
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+				},
+			},
+		})
+
+		telescope.load_extension("fzf")
+
+		-- keymaps
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Telescope live grep" })
 		vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "Telescope buffers" })
